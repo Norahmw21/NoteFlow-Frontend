53eb2e5a3efac81137dd50733db73180446ff895
Norahmw21 <norahmw2@gmail.com>
2025-08-24 11:30:42 +0300

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module com.example.noteflowfrontend {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.web;                   // ✅ for HTMLEditor
 
     requires com.dlsc.formsfx;
     requires java.net.http;
     requires com.fasterxml.jackson.databind;
     requires java.prefs;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
     opens com.example.noteflowfrontend to javafx.fxml;
     opens com.example.noteflowfrontend.core.dto to com.fasterxml.jackson.databind;
